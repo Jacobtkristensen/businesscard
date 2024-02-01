540796ed289eafa4401f773ad4015e00384cdd10
Jacobtkristensen <115540527+Jacobtkristensen@users.noreply.github.com>
2024-02-01 10:43:47 +0100

    okæ

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image,} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <div style={styles.div1}>
+      <Text></Text>
+      <Image style={styles.img} source = {{uri:"https://i.ebayimg.com/images/g/iDcAAOSwo9VbDbfn/s-l500.jpg"}}/>
+      <Image style={styles.img2} source = './images/bperson.png'/>
+
+      </div>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +22,24 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+   img: {
+    width: 150,
+    height: 100,
+    position: 'absolute',
+    bottom: -30,
+    right: -10,
+    },
+  img2: {
+   width: 150,
+    height: 100,
+    position: 'absolute',
+    top: 10,
+    left: -10,
+    },
+  div1: {
+    width: 300,
+    height: 150,
+    backgroundColor: '#fff',
   },
 });
